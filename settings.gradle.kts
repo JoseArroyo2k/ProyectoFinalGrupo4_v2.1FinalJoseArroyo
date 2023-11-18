def2c326c7547e51b02b23bf77c6ab3a0888169d
laboratorioesan <laboratorioesan@esan.edu.pe>
2023-11-18 12:14:33 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ProyectoFinalGrupo4_v2.1Fin"
+include(":app")
+ 
