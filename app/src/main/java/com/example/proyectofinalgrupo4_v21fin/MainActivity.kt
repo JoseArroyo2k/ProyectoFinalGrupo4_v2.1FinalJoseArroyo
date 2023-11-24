a4eb1c312c1d482e526dda8324e45f9618aa8339
Administrador <fasfsa@gmail.com>
2023-11-24 00:24:26 -0500

    DSADAS

--- app/src/main/java/com/example/proyectofinalgrupo4_v21fin/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinalgrupo4_v21fin/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +23,31 @@ class MainActivity : AppCompatActivity() {
         btnReg.setOnClickListener {
             startActivity(Intent(this,RegisterActivity::class.java))
 
+        }
+
+        btnLogin.setOnClickListener {
+            val email = txtCorreo.text.toString()
+            val password = txtClave.text.toString()
+
+            auth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this){task->
+                    if(task.isSuccessful){
+                        Snackbar
+                            .make(
+                                findViewById(android.R.id.content)
+                                ,"Inicio de sesión exitoso"
+                                , Snackbar.LENGTH_LONG
+                            ).show()
+                        startActivity(Intent(this, NavigationActivity::class.java))
+                    }else {
+                        Snackbar
+                            .make(
+                                findViewById(android.R.id.content)
+                                ,"Credenciales inválidas"
+                                , Snackbar.LENGTH_LONG
+                            ).show()
+                    }
+                }
         }
 
     }
